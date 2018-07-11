3bdb37b76bfe41f470ee281d5216931a34e26f07
Ignacio Sambataro <ignaciosamba@gmail.com>
2018-07-11 11:53:48 -0300

    Third class.
    Adding button to MovieAdapter to share the title with
    a Chooser.
    
    Signed-off-by: Ignacio Sambataro <ignaciosamba@gmail.com>

--- app/src/main/java/com/example/isambataro/lesson2/view/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/isambataro/lesson2/view/adapter/MovieAdapter.java
@@ -1,6 +1,8 @@
 package com.example.isambataro.lesson2.view.adapter;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,8 +11,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.isambataro.lesson2.ApplicationContext;
 import com.example.isambataro.lesson2.R;
 import com.example.isambataro.lesson2.model.DataMovies;
+import com.example.isambataro.lesson2.view.activity.BaseActivity;
 
 import java.util.List;
 
@@ -18,15 +22,15 @@ import java.util.List;
  * Created by isambataro on 18/06/18.
  */
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> implements View.OnClickListener{
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>/* implements View.OnClickListener*/{
 
     private List<DataMovies> items;
-    private OnItemClickListener mListener;
+//    private OnItemClickListener mListener;
 
 
-    public interface OnItemClickListener {
-        void onItemClick(View view, int item);
-    }
+//    public interface OnItemClickListener {
+//        void onItemClick(View view, int item);
+//    }
 
     public MovieAdapter (List<DataMovies> movieList/*, OnItemClickListener listener*/){
         super();
@@ -39,10 +43,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
         notifyDataSetChanged();
     }
 
-    @Override
-    public void onClick(View view) {
-        mListener.onItemClick(view, getItemCount());
-    }
+//    @Override
+//    public void onClick(View view) {
+//        mListener.onItemClick(view, getItemCount());
+//    }
 
     @NonNull
     @Override
@@ -81,6 +85,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
         private TextView episode;
         private TextView producer;
         private TextView director;
+        private FloatingActionButton floatingActionButton;
 
         public MovieViewHolder(View v){
             super(v);
@@ -90,6 +95,21 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
             episode = v.findViewById(R.id.episode);
             producer = v.findViewById(R.id.producer);
             director = v.findViewById(R.id.director);
+            floatingActionButton = v.findViewById(R.id.fab);
+            floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent share = new Intent(android.content.Intent.ACTION_SEND);
+                    share.setType("text/plain");
+                    share.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+
+                    // Add data to the intent, the receiving app will decide
+                    // what to do with it.
+                    share.putExtra(Intent.EXTRA_TEXT, title.getText());
+
+                    ApplicationContext.getAppContext().startActivity(Intent.createChooser(share, "Share link!"));
+                }
+            });
             itemView.setOnClickListener(this);
         }
 
@@ -98,6 +118,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
             Toast.makeText(view.getContext(), "value = " + title.getText().toString(),
                     Toast.LENGTH_LONG).show();
         }
+
     }
 
 }
